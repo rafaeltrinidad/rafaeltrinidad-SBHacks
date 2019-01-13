bf85659084c3c6316ae06bf9bbf0a3e21b3330a8
Rain <rainaininn@gmail.com>
2019-01-13 00:54:43 -0800

    Added QUIT button.

--- SportsTrackerApp/app/src/main/java/com/example/rafael/sportstrackerapp/LengthsActivity.java
+++ SportsTrackerApp/app/src/main/java/com/example/rafael/sportstrackerapp/LengthsActivity.java
@@ -5,20 +5,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class LengthsActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lengths);
 
-
-        Button button = (Button) findViewById(R.id.next_button);
-        button.setOnClickListener(new View.OnClickListener() {
+        Button button_next = (Button) findViewById(R.id.next_button);
+        button_next.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+
+                /*EditText team1name = (EditText) findViewById(R.id.team1_fill);
+                EditText team2name = (EditText) findViewById(R.id.team2_fill);
+                TextView team1out = (TextView)(MainActivity) findViewById(R.id.Team_1);
+                TextView team2out = (TextView)(MainActivity) findViewById(R.id.Team_2);
+                MainActivity obj = new MainActivity();
+                TextView team1out = obj.getTextView1();
+                TextView team2out = obj.getTextView2();
+                team1out.setText(team1name.getText().toString());
+                team2out.setText(team2name.getText().toString());*/
                 startActivity(new Intent(LengthsActivity.this, MainActivity.class));
+
             }
         });
     }
+
 }
--- SportsTrackerApp/app/src/main/java/com/example/rafael/sportstrackerapp/MainActivity.java
+++ SportsTrackerApp/app/src/main/java/com/example/rafael/sportstrackerapp/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.rafael.sportstrackerapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,10 +14,33 @@ public class MainActivity extends AppCompatActivity {
     private int leftCount = 0;
     private int rightCount = 0;
 
+    /*EditText team1name;
+    EditText team2name;
+    TextView team1out;
+    TextView team2out;*/
+
+    public TextView getTextView1()
+    {
+        TextView txtView = (TextView)findViewById(R.id.Team_1);
+        return txtView;
+    }
+    public TextView getTextView2()
+    {
+        TextView txtView = (TextView)findViewById(R.id.Team_2);
+        return txtView;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*team1name = (EditText) findViewById(R.id.team1_fill);
+        team2name = (EditText) findViewById(R.id.team2_fill);
+        team1out = (TextView) findViewById(R.id.Team_1);
+        team2out = (TextView) findViewById(R.id.Team_2);
+        team1out.setText(team1name.getText());
+        team2out.setText(team2name.getText());*/
 
         Button button_L1 = (Button) findViewById(R.id.left1);
         button_L1.setOnClickListener(new View.OnClickListener() {
@@ -51,6 +76,13 @@ public class MainActivity extends AppCompatActivity {
         button_R3.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 alterScore(false, 3);
+            }
+        });
+
+        Button button_quit = (Button) findViewById(R.id.quit);
+        button_quit.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LengthsActivity.class));
             }
         });
     }
